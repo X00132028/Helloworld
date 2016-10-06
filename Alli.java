c837243f63a38c175e38e63dd53e5a6ce8d8b77b
X00132028 <X00132028@computing.stu.it-tallaght.ie>
2016-10-06 09:51:48 +0100

    My Program

--- Alli.java
+++ Alli.java
@@ -1,8 +1,9 @@
 public class Alli{
 
 public static void main(String [] args){
-System.out.println("My Name is Alax");
-
+System.out.println("My Name is Alex");
+System.out.println("Welcome");
+System.out.println("Hello world");
 
 }
 }
